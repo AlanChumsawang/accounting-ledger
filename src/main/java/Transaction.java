0485176a7073d3c9bed0966d2ceffffb9063a50c
AlanChumsawang <alan.chumsawang@outlook.com>
2024-10-15 11:54:30 -0400

    Created the Transaction class defining the Transaction type and developed necessary getters

---
+++ src/main/java/Transaction.java
@@ -0,0 +1,35 @@
+public class Transaction {
+    private final String date;
+    private final String time;
+    private final String description;
+    private final String vendor;
+    private final double amount;
+
+    public Transaction(String date, String time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
--- src/main/java/TransactionManager.java
+++ src/main/java/TransactionManager.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class TransactionManager {
     public void addDeposit() {
         System.out.println("Deposit added");
