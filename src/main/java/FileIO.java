dfce76cf872e7f5e715b1d2dfd1c383b9bf7f10c
AlanChumsawang <alan.chumsawang@outlook.com>
2024-10-15 11:38:35 -0400

    Implemented File IO logic to read and write to csv

--- src/main/java/FileIO.java
+++ src/main/java/FileIO.java
@@ -1,2 +1,24 @@
+import java.io.*;
+
 public class FileIO {
+
+    public static void readCSV(String filename) {
+        try {
+            FileReader fileReader = new FileReader("transactions.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+        }
+        catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+
+    public static void writeToCSV(String filename) {
+        // Write to CSV
+        try {
+            FileWriter fileWriter = new FileWriter("transactions.csv");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
 }
