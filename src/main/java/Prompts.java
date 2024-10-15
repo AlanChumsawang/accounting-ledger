bcdebcfadc299a55da91995209e4be3cc46cae9c
AlanChumsawang <alan.chumsawang@outlook.com>
2024-10-15 11:11:32 -0400

    Created Prompt class & added main menu prompt

---
+++ src/main/java/Prompts.java
@@ -0,0 +1,28 @@
+public class Prompts {
+    static String Logo = """
+             __  __                          \s
+            |  \\/  |_   _                    \s
+            | |\\/| | | | |                   \s
+            | |  | | |_| |                   \s
+            |_|  |_|\\__, |                   \s
+             _      |___/  _                 \s
+            | |    ___  __| | __ _  ___ _ __ \s
+            | |   / _ \\/ _` |/ _` |/ _ | '__|\s
+            | |__|  __| (_| | (_| |  __| |   \s
+            |_____\\___|\\__,_|\\__, |\\___|_|   \s
+                             |___/           \s
+            
+            
+            """;
+    static String mainMenu = """
+            D) Add Deposit
+            P) Make Payment
+            L) Ledger
+            X) Exit
+            """;
+    
+
+
+
+}
+
