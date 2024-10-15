7fc7baa0c65541684b35d0534854cdab4ff3f644
AlanChumsawang <alan.chumsawang@outlook.com>
2024-10-15 11:31:55 -0400

    Created a FileIO Class to read and write to csv file

--- src/main/java/TransactionManager.java
+++ src/main/java/TransactionManager.java
@@ -1,3 +1,11 @@
 public class TransactionManager {
+    public void addDeposit() {
+        System.out.println("Deposit added");
+    }
+
+    public void makePayment() {
+        System.out.println("Payment made");
+    }
+
 
 }
