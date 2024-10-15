0d05a68c6393981153791195a4a06b8aaf2f1ced
AlanChumsawang <alan.chumsawang@outlook.com>
2024-10-15 11:47:00 -0400

    Implemented file reading logic for csv reading

--- src/main/java/FileIO.java
+++ src/main/java/FileIO.java
@@ -5,7 +5,19 @@ public class FileIO {
     public static void readCSV(String filename) {
         try {
             FileReader fileReader = new FileReader("transactions.csv");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            BufferedReader bufReader = new BufferedReader(fileReader);
+            String currentLine = bufReader.readLine();
+            while ( (currentLine = bufReader.readLine()) !=null){
+                String [] transactionInfo = currentLine.split("[|]");
+                String date = transactionInfo[0];
+                String time = transactionInfo[1];
+                String description = transactionInfo[2];
+                String vendor = transactionInfo[3];
+                String amount = transactionInfo[4];
+                // Transaction transaction = new Transaction(date, time, description, vendor, amount);
+                // TransactionManager.addTransaction(transaction);
+            }
+
         }
         catch (IOException e) {
             System.out.println("File not found");
